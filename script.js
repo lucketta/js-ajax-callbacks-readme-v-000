f7351f5db2f76d22408759067385289d491d054f
lucketta <lucketta@umich.edu>
2017-07-26 01:21:39 -0400

    Done.

---
+++ script.js
@@ -0,0 +1,17 @@
+// We should wait for the page to load before running our Ajax request
+$(document).ready(function(){
+  // Now we start the Ajax GET request. The first parameter is the URL with the data.
+  // The second parameter is a function that handles the response.
+  // $.get("sentence.html", function(response) {
+  //   // Here we are getting the element on the page with the id of sentences and
+  //   // inserting the response
+  //   $("#sentences").html(response);
+  // });
+  var url = "https://api.github.com/repos/rails/rails/commits?sha=82885325e04d78fb7ec608a4670164d842d23078";
+
+  $.get(url)
+    .done(function(data) {
+      console.log("Done");
+      console.log(data);
+    });
+});
